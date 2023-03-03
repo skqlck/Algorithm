624346bb362fffb36675337325c5ffc17d2e6c43
bellup <skqlck@gmail.com>
2023-03-03 17:58:27 +0900

    2023-03-03Algorithm

--- Backtracking/B17136.py
+++ Backtracking/B17136.py
@@ -1,7 +1,7 @@
 def can_attach(x,y,k):
     for nx in range(x,x+k):
         for ny in range(y,y+k):
-            if 0 > nx or 0 > ny or nx >= 10 or ny >= 10:
+            if nx > 9 or ny > 9:
                 return False
             if paper[nx][ny] == 0:
                 return False
@@ -14,23 +14,33 @@ def detach(x,y,k):
     for nx in range(x,x+k):
         for ny in range(y,y+k):
             paper[nx][ny] = 1
-def dfs(depth,cnt):
+
+def dfs(x,y,depth,cnt):
     global answer
     if depth >= answer:
         return
     if cnt == 0:
-        answer = min(answer,depth)
+        answer = min(depth, answer)
         return
-    for x in range(10):
-        for y in range(10):
-            if paper[x][y] == 1:
-                for k in range(5,0,-1):
-                    if color_paper[k] and can_attach(x,y,k):
-                        attach(x,y,k)
-                        color_paper[k] -= 1
-                        dfs(depth+1,cnt-k**2)
-                        detach(x,y,k)
-                        color_paper[k] += 1
+
+    if paper[x][y] == 1:
+        for k in range(5,0,-1):
+            if color_paper[k] and can_attach(x,y,k):
+                attach(x,y,k)
+                color_paper[k] -= 1
+                if y+k < 10:
+                    dfs(x, y+k, depth+1, cnt-k**2)
+                elif x < 9:
+                    dfs(x+1, 0, depth+1, cnt-k**2)
+                else:
+                    answer = min(depth+1,answer)
+                detach(x,y,k)
+                color_paper[k] += 1
+    else:
+        if y<9:
+            dfs(x,y+1,depth,cnt)
+        elif x<9:
+            dfs(x+1,0,depth,cnt)
 
 paper = [list(map(int,input().split())) for _ in range(10)]
 total = 0
@@ -40,19 +50,8 @@ for i in range(10):
             total += 1
 color_paper = [0,5,5,5,5,5]
 answer = 25
-if total:
-    for x in range(10):
-        for y in range(10):
-            if paper[x][y] == 1:
-                for k in range(5,0,-1):
-                    if color_paper[k] and can_attach(x,y,k):
-                        attach(x,y,k)
-                        color_paper[k] -= 1
-                        dfs(1,total-k**2)
-                        detach(x,y,k)
-                        color_paper[k] += 1
-                if answer == 25:
-                    print(-1)
-                else:
-                    print(answer)
-                exit(0)
+dfs(0,0,0,total)
+if answer == 25:
+    print(-1)
+else:
+    print(answer)
