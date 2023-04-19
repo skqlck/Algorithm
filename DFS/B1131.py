1f70ae8a014530c974189c7ea188272e90ccb8d0
bellup <skqlck@gmail.com>
2023-04-20 01:25:47 +0900

    platinum solve...

--- DFS/B1131.py
+++ DFS/B1131.py
@@ -1,20 +1,54 @@
 A,B,K = map(int,input().split())
+power = [i**K for i in range(10)]
+DP = [0]*(3188647)
+def getNextSeq(number):
+    return sum(map(lambda x: power[int(x)], str(number)))
+
 answer = 0
+for number in range(A,B+1):
+    if not DP[number]:
+        path = {}
+        path[number] = 1
+        now = number
+        while True:
 
-for i in range(A,B+1):
-    DP = set()
-    i = sorted(str(i))
-    DP.add(i)
-    while True:
-        j = sum(map(lambda x:int(x)**K,i))
-        if j == A:
-            answer += A
-            break
-        j = sorted(str(i))
-        if j in DP:
-            answer += min(DP)
-            break
-        DP.add(j)
-        i = j
+            next = getNextSeq(now)
+
+            if DP[next]: # 기존 방문한 것들과 만남
+                minV = DP[next]
+                list_path = list(path.keys())
+                for idx in range(-1,-len(path)-1,-1):
+                    before = list_path[idx]
+                    if before < minV:
+                        minV = before
+                    DP[before] = minV
+                break
+
+            if next in path: # 사이클 형성
+                list_path = list(path.keys())
+                Cycle = set()
+                Cycle.add(next)
+                CycleMinV = next
+                while True:
+                    next = getNextSeq(next)
+                    if next in Cycle:
+                        break
+                    Cycle.add(next)
+                    if next < CycleMinV:
+                        CycleMinV = next
+
+                for node in Cycle:
+                    DP[node] = CycleMinV
+
+                for idx in range(len(list_path) - len(Cycle) - 1,-1,-1):
+                    if list_path[idx] < CycleMinV:
+                        CycleMinV = list_path[idx]
+                    DP[list_path[idx]] = CycleMinV
+                break
+            # visited[next] = 1
+            path[next] = 1
+            now = next
+
+    answer += DP[number]
+
 print(answer)
-
